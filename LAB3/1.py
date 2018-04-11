6ce63d085275d70549d2bb82a64f7a5a86d8c7b0
“boy07132004” <boy07132004@gmail.com>
2018-04-11 18:37:38 +0800

    0637

--- LAB3/1.py
+++ LAB3/1.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python3
 """
-This program is for RaspberryPi(R) to get DHT11's data with python3.
-Notice that you have to install Adafruit_DHT module first with bellow:
-
-    $ git clone https://github.com/adafruit/Adafruit_Python_DHT.git
-    $ cd Adafruit_DHT
-    $ sudo apt-get update
-    $ sudo apt-get install build-essential python-dev
-    $ sudo python3 setup.py install
-
 @author: FATESAIKOU
 @argv[1]: BCM pin id
 @argv[2]: monit time
@@ -16,22 +7,30 @@ Notice that you have to install Adafruit_DHT module first with bellow:
 """
 
 import time
+import argparse
 import sys
-import Adafruit_DHT as DHT
+
+#import Adafruit_DHT as DHT
 
 
 def main():
-    BCM_PIN    = int(sys.argv[1])
-    TOTAL_TIME = float(sys.argv[2])
-    DELAY      = float(sys.argv[3])
-
-
-    iter_num = int(TOTAL_TIME / DELAY)
-    for i in range(iter_num):
+    P       =sys.argv[1]
+    BCM_PIN =int(P[1:len(P)-1])
+    co      =sys.argv[2]
+    cond    =( co [1:len(co)-1] )
+    mt      =sys.argv[3]
+    monit_t =float(mt[1:len(mt)-1])
+        
+    run = monit_t / 5
+    for i in range(run) :   
         h, t = DHT.read_retry(11, BCM_PIN)
-        print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h))
+        if eval(cond):
+            print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h))
+        else:break
+        time.sleep(5)
+    #if co == True:print("T")
+    #else:print("F")
 
-        time.sleep(DELAY)
 
 if __name__ == "__main__":
     main()
