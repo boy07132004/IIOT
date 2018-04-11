69177a6910768f4b01609e9703e6625c2ff542a3
boy07132004 <boy07132004@gmail.com>
2018-04-11 18:44:13 +0800

    ras

--- LAB3/1.py
+++ LAB3/1.py
@@ -7,21 +7,21 @@
 """
 
 import time
-import argparse
+
 import sys
 
-#import Adafruit_DHT as DHT
+import Adafruit_DHT as DHT
 
 
 def main():
     P       =sys.argv[1]
     BCM_PIN =int(P[1:len(P)-1])
     co      =sys.argv[2]
-    cond    =( co [1:len(co)-1] )
+    cond    =(co[1:len(co)-1])
     mt      =sys.argv[3]
     monit_t =float(mt[1:len(mt)-1])
         
-    run = monit_t / 5
+    run = int(monit_t / 5)
     for i in range(run) :   
         h, t = DHT.read_retry(11, BCM_PIN)
         if eval(cond):
