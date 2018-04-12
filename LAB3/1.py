6de3e466602cfb4443a79f3dd94475a6615e6e4f
“boy07132004” <boy07132004@gmail.com>
2018-04-12 15:13:15 +0800

    0313

--- LAB3/1.py
+++ LAB3/1.py
@@ -10,7 +10,7 @@ import time
 
 import sys
 
-import Adafruit_DHT as DHT
+#import Adafruit_DHT as DHT
 
 
 def main():
@@ -19,18 +19,18 @@ def main():
     co      =sys.argv[2]
     cond    =(co[1:len(co)-1])
     mt      =sys.argv[3]
-    monit_t =float(mt[1:len(mt)-1])
-        
+    monit_t =(mt[1:len(mt)-1])
+    
     run = int(monit_t / 5)
+        
     for i in range(run) :   
         h, t = DHT.read_retry(11, BCM_PIN)
-        if eval(cond):
+        wh={'t':t,'h':h}
+        if eval(cond,wh):
             print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(t, h))
         else:break
         time.sleep(5)
-    #if co == True:print("T")
-    #else:print("F")
-
+    
 
 if __name__ == "__main__":
     main()
