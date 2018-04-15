4b366338cac458d6547d49240a1b315546c93ccc
boy07132004 <37643421+boy07132004@users.noreply.github.com>
2018-04-15 15:04:03 +0800

    Add files via upload

---
+++ LAB3/monitorDHT.py
@@ -0,0 +1,59 @@
+import time
+import sys
+import Adafruit_DHT as DHT
+import subprocess as sp
+import logging
+
+
+#-------------------------------LOG---------------------------------#
+logging.basicConfig(level=logging.DEBUG,filename='log.txt', \
+format='%(asctime)s -%(name)s : %(message)s')
+L = logging.getLogger('STDOUT')
+R = logging.getLogger('RETURN')
+LER= logging.getLogger('STDERR')
+#-------------------------------------------------------------------#
+
+
+def main():
+    delay   =5
+    P       =sys.argv[1]
+    BCM_PIN =int(P[1:len(P)-1])
+    co      =sys.argv[2]
+    cond    =co[1:len(co)-1]
+    mt      =sys.argv[3]
+    monit_t =float(mt[1:len(mt)-1])
+    cb      =sys.argv[4]
+    callback=cb[1:len(cb)-1].split()
+    r       =int(monit_t // delay)
+
+
+    #-----whitelist for command-----#
+    whitelist=['ls','./test0.py','./test1.py']
+    #-------------------------------#
+    
+    if r >= 0 :   run = r+1  
+    else:LER.error('error monit_time')
+    
+
+    for i in range(run) :   
+        
+        if i==run-1:    delay=monit_t % 5
+        
+        h, t = DHT.read_retry(11, BCM_PIN)
+        h=h/100
+        wh={'t':t,'h':h}
+
+        if eval(cond,{"__builtins__":None},wh):
+            if set(callback)&set(whitelist): 
+                out=sp.Popen(callback,stdout=sp.PIPE,stderr=sp.PIPE)
+                (stdout,stderr)=out.communicate()
+                R.debug(out.returncode)
+                if stdout:L.debug(stdout)
+                if stderr:LER.error(stderr)
+            else:LER.error('error in callback')
+        else:LER.error('error in condition')
+        time.sleep(delay)
+
+
+if __name__ == "__main__":
+    main()
