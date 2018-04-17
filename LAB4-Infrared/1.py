1c2da28ee1208ad6c5a1ac8ae67e2482141a82dc
“boy07132004” <boy07132004@gmail.com>
2018-04-17 17:07:30 +0800

    gitpush

--- LAB4-Infrared/1.py
+++ LAB4-Infrared/1.py
@@ -3,6 +3,7 @@ import time
 import sys
 import json
 
+#-----------------------DEF------------------------#
 def initEnv(pin):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
@@ -13,7 +14,6 @@ def initPin(lp):
 
 def endEnv():
     GPIO.cleanup()
-
 
 def getSignal(pin):
     start, stop = 0, 0
@@ -33,7 +33,6 @@ def getSignal(pin):
     
         signals.append(duringUp)
 
-
 def compairSignal(s1, s2, rang):
     min_len = min(len(s1), len(s2))
 
@@ -49,32 +48,47 @@ def decodeSingal(s, signal_map, rang):
             return name
 
     return None
-
-    
-
+#-----------------------DEF------------------------#
 def main():
-    PIN = int(sys.argv[1])
-    led = int(sys.argv[2])
-    SIGNAL_MAP = sys.argv[3]
-        
-    src = open(SIGNAL_MAP, 'r')
+    # read argv
+    p1  = sys.argv[1]
+    l1  = sys.argv[2]
+    m1  = sys.argv[3]
+    PIN = p1[1:len(p1)-1]
+    led = l1[1:len(l1)-1]
+    smap= m1[1:len(m1)-1]
+    
+    # open map
+    src = open(smap, 'r')
     signal_map = json.loads(src.read())
     src.close()
 
+    # init PIN
     initEnv(PIN)
     initPin(led)
+    
+    # start .........
     while True:
-        print("1:setup led\n   0:setoff led\n P:poweroff\n")
+    
+    # function
+        print("1:	setup led\n0	:setoff led\nPower:poweroff\n")
         s = getSignal(PIN)
         sig=decodeSingal(s,signal_map,0.001)
         
-        print('\n',sig)
-        if sig == str('on'):
-            GPIO.output(led,True)
-        elif sig == str('off'):
-            GPIO.output(led,False)
+        if sig == str('on'):GPIO.output(led,True)
+        
+        elif sig == str('off'):GPIO.output(led,False)
+        
         elif sig == str('power'):break
+        
+
+
+
+
         else:print('not record')
-    endEnv()    
+    
+    # end
+    endEnv()
+
 if __name__ == "__main__":
     main()
