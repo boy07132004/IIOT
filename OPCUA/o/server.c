a72df34bd0868d3dee6e27e3e1bede5b965245fe
boy07132004 <boy07132004@gmail.com>
2018-05-08 17:50:51 +0800

    tolab

--- OPCUA/o/server.c
+++ OPCUA/o/server.c
@@ -1,6 +1,7 @@
 #include <signal.h>
 #include "open62541.h"
 
+
 UA_Boolean running = true;
 static void stopHandler(int sig) {
     UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "received ctrl-c");
@@ -15,8 +16,11 @@ int main(void) {
     UA_Server *server = UA_Server_new(config);
 
     UA_StatusCode retval = UA_Server_run(server, &running);
-    UA_String ID("123")
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER, ID);
+    //const char* IDn = "66";
+    //const char *ID = "66";
+    //UA_String ID="N66061359";
+    UA_String st("TES");
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,st);
     UA_Server_delete(server);
     UA_ServerConfig_delete(config);
     return (int)retval;
