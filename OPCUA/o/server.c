435cd5260ab995643076186caf306be08a142084
boy07132004 <boy07132004@gmail.com>
2018-05-08 10:43:55 +0800

    o

--- OPCUA/o/server.c
+++ OPCUA/o/server.c
@@ -1,26 +1,21 @@
 #include <signal.h>
 #include "open62541.h"
-#include <stdio.h>
 
 UA_Boolean running = true;
-
-static void stopHandler(int sig)
-{
-	UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "received ctrl-c");
-	running = false;
+static void stopHandler(int sig) {
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "received ctrl-c");
+    running = false;
 }
 
-int main(void)
-{
-	signal(SIGINT, stopHandler);
-	signal(SIGTERM,stopHandler);
-	UA_ServerConfig *config = UA_ServerConfig_new_default();
-	UA_ServerNetworkLayer nl = UA_ServerNetworkLayerTCP(UA_ConnectionConfig_standard,4840);
-	config->networkLayers = &nl;
-	config->networkLayersSize = 1;
-	UA_Server *server = UA_Server_new(config);
-	UA_Server_run(server,&running);
-	UA_Server_delete(server);
-	nl.deleteMembers(&nl);
-	return 0 ;
+int main(void) {
+    signal(SIGINT, stopHandler);
+    signal(SIGTERM, stopHandler);
+
+    UA_ServerConfig *config = UA_ServerConfig_new_default();
+    UA_Server *server = UA_Server_new(config);
+
+    UA_StatusCode retval = UA_Server_run(server, &running);
+    UA_Server_delete(server);
+    UA_ServerConfig_delete(config);
+    return (int)retval;
 }
