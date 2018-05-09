686ac5908de9a2af486bfc35081e6d591b1aece6
boy07132004 <boy07132004@gmail.com>
2018-05-09 13:13:43 +0800

    finsh1 0509

--- OPCUA/o/server.c
+++ OPCUA/o/server.c
@@ -1,6 +1,6 @@
 #include <signal.h>
 #include "open62541.h"
-#include <stdio.h>
+
 
 
 UA_Boolean running = true;
@@ -17,12 +17,13 @@ int main(void) {
     UA_Server *server = UA_Server_new(config);
     
     const char* c = "N66061359";
+    UA_DateTimeStruct t = UA_DateTime_toStruct(UA_DateTime_now());
     UA_String s = UA_String_fromChars(c);
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,"%s",s.data);
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,"%s logging...",s.data);
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,"StartTime:%04u-%02u-%02u %02u:%02u:%02u",t.year,t.month,t.day,t.hour+8,t.min,t.sec);
     
-    UA_DateTime Ser_t;
-    Ser_t = UA_DateTime_now();
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,"%u",Ser_t);
+    
+    //UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,"%s",UA_DateTime_toString(Ser_t));
 
     UA_StatusCode retval = UA_Server_run(server, &running); 
     UA_Server_delete(server);
