4a7602c71b7e3445d568bc36ef2388f568be83fb
boy07132004 <boy07132004@gmail.com>
2018-05-09 14:55:50 +0800

    123

--- OPCUA/o/server.c
+++ OPCUA/o/server.c
@@ -16,14 +16,15 @@ int main(void) {
     UA_ServerConfig *config = UA_ServerConfig_new_default();
     UA_Server *server = UA_Server_new(config);
     
-    const char* c = "N66061359";
-    UA_DateTimeStruct t = UA_DateTime_toStruct(UA_DateTime_now());
-    UA_String s = UA_String_fromChars(c);
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,"%s logging...",s.data);
-    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,"StartTime:%04u-%02u-%02u %02u:%02u:%02u",t.year,t.month,t.day,t.hour+8,t.min,t.sec);
+    //-----------STUDENT_ID------------//
+    const char* student = "N66061359";
+    UA_String ID = UA_String_fromChars(student);
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,"%s logging...",ID.data);
+    //------------STARTTIME------------//
+    UA_DateTimeStruct t = UA_DateTime_toStruct(UA_DateTime_now()+UA_DateTime_localTimeUtcOffset());   
+    UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,"StartTime:%04u-%02u-%02u %02u:%02u:%02u",t.year,t.month,t.day,t.hour,t.min,t.sec);
     
-    
-    //UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_SERVER,"%s",UA_DateTime_toString(Ser_t));
+    //---------------END---------------//
 
     UA_StatusCode retval = UA_Server_run(server, &running); 
     UA_Server_delete(server);
