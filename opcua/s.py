349c6a656a08bdbb289efc474a9515ce3096eede
boy07132004 <boy07132004@gmail.com>
2018-05-17 17:06:09 +0800

    123

--- opcua/s.py
+++ opcua/s.py
@@ -3,8 +3,21 @@ def fuc(you):
     you=you%4
     if you==1:
         print('r')
+        pwmr.ChangeDutyCycle(90)
+        pwmg.ChangeDutyCycle(0)
+        pwmb.ChangeDutyCycle(0)
     elif you==2:
         print('g')
+        pwmr.ChangeDutyCycle(0)
+        pwmg.ChangeDutyCycle(90)
+        pwmb.ChangeDutyCycle(0)
     elif you==3:
         print('b')
-    else:print('close')
+        pwmr.ChangeDutyCycle(0)
+        pwmg.ChangeDutyCycle(0)
+        pwmb.ChangeDutyCycle(90)
+    else:
+        print('close')
+        pwmr.ChangeDutyCycle(0)
+        pwmg.ChangeDutyCycle(0)
+        pwmb.ChangeDutyCycle(0)
