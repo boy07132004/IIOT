9e608034dd42bfb7233b1fac7d89b26195534b55
boy07132004 <boy07132004@gmail.com>
2018-05-24 15:16:44 +0800

    c

--- opcua/LED.py
+++ opcua/LED.py
@@ -5,12 +5,10 @@ import RPi.GPIO as GPIO
 def main(LED):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(7, GPIO.OUT)
-    while LED:
-        GPIO.output(7, True)
+    GPIO.output(7, True)
 def OFF(LED):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(7, GPIO.OUT)
-    while LED:
-        GPIO.output(7, False)
+    GPIO.output(7, False)
 def clean():
     GPIO.cleanup()
