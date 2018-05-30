195d5f4cf136db33e61d3b52eb4fc93a4aa0d015
boy07132004 <boy07132004@gmail.com>
2018-05-30 21:46:31 +0800

    0530end

--- opcua/s4.c
+++ opcua/s4.c
@@ -1,17 +1,21 @@
 #include </usr/include/python3.5/Python.h>
 #include <signal.h>
 #include "open62541.h"
+
 //  Add variable
 //  Handler
-    static void handler(UA_UInt32 monID,UA_DataValue *value,void *context){
-        UA_Variant value1;
-        UA_Variant_init(&value1);
-        const UA_NodeId nodeId= UA_NODEID_STRING(0, "DHT-Variable");
-        UA_StatusCode retval = UA_Client_readValueAttribute(context,nodeId,&value1);
-        if (retval == UA_STATUSCODE_GOOD){
-            UA_Double Hvalue = *(UA_Double*)value1.data;
-            printf("Humidity : %f \n",Hvalue);
-        }
+
+    static void handler_HUMChanged(UA_Client *client, UA_UInt32 subId, void *subContext,
+                           UA_UInt32 monId, void *monContext, UA_DataValue *value) {
+    if(UA_Variant_hasScalarType(&value->value, &UA_TYPES[UA_TYPES_DOUBLE])) {
+        UA_Double Hdata = *(UA_Double *) value->value.data;
+        printf("Humidity: %f \n",Hdata);}}
+
+    static void handler_TMPChanged(UA_Client *client, UA_UInt32 subId, void *subContext,
+                           UA_UInt32 monId, void *monContext, UA_DataValue *value) {
+    if(UA_Variant_hasScalarType(&value->value, &UA_TYPES[UA_TYPES_DOUBLE])) {
+        UA_Double Tdata = *(UA_Double *) value->value.data;
+        printf("Temperature: %f \n",Tdata);}
     }
 //  Stop Function
     UA_Boolean running = true;
@@ -20,6 +24,13 @@
         running = false;
     }
 
+//  default
+    static void deleteSubscriptionCallback(UA_Client *client, UA_UInt32 subscriptionId, void *subscriptionContext) {
+    printf("Bye Bye\n");
+    }
+    static void subscriptionInactivityCallback (UA_Client *client, UA_UInt32 subId, void *subContext) {
+    printf("Inactivity for subscription\n");}
+    
 
 int main(void) {
     signal(SIGINT, stopHandler);
@@ -28,17 +39,27 @@ int main(void) {
     UA_StatusCode retval = UA_Client_connect(client, "opc.tcp://192.168.50.93:4840");
     if(retval != UA_STATUSCODE_GOOD) {UA_Client_delete(client);return (int)retval;}
     //
-    const UA_NodeId HumID = UA_NODEID_STRING(0, "DHT-Variable");
-    const UA_NodeId TmpID = UA_NODEID_STRING(1, "DHT-Variable");
-    UA_Variant Humvalue,Tmpvalue;
-    UA_Variant_init(&Humvalue);
-    UA_Variant_init(&Tmpvalue);
-    UA_UInt32 subID = 0;
-    UA_UInt32 monID = 0;
- 
-
-    while(running){
+    UA_CreateSubscriptionRequest
+        request = UA_CreateSubscriptionRequest_default();
+    UA_CreateSubscriptionResponse
+        response = UA_Client_Subscriptions_create(client, request,NULL, NULL, deleteSubscriptionCallback);
     
+        UA_MonitoredItemCreateRequest
+        humRequest = UA_MonitoredItemCreateRequest_default(UA_NODEID_STRING(0, "DHT-Variable"));
+        UA_MonitoredItemCreateResult
+        humResponse = UA_Client_MonitoredItems_createDataChange(client, response.subscriptionId,UA_TIMESTAMPSTORETURN_BOTH,
+                                             humRequest, NULL,handler_HUMChanged, NULL);
+   
+        UA_MonitoredItemCreateRequest
+            tmpRequest = UA_MonitoredItemCreateRequest_default(UA_NODEID_STRING(1, "DHT-Variable"));
+        UA_MonitoredItemCreateResult
+            tmpResponse = UA_Client_MonitoredItems_createDataChange(client, response.subscriptionId,UA_TIMESTAMPSTORETURN_BOTH,
+                                             tmpRequest, NULL, handler_TMPChanged, NULL);
+     
+    //
+    while(running){
+        UA_StatusCode retval = UA_Client_connect(client, "opc.tcp://192.168.50.93:4840");
+        UA_Client_runAsync(client, 1000);
     }
 
     UA_Client_disconnect(client);
