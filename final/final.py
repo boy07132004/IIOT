835d5bcb07310f009742aa95eaf0b2601c6b4d39
boy07132004 <boy07132004@gmail.com>
2018-06-18 16:38:18 +0800

    dht

--- final/final.py
+++ final/final.py
@@ -1,129 +1,44 @@
 #!/usr/bin/env python3
-"""
-RGB_pin->3,5
-Ir_pin->37
-"""
 import RPi.GPIO as GPIO
+import signal
+import Adafruit_DHT as DHT
 import time
-import sys
-import json
 
-#-----------------------DEF------------------------#
+dhting = True
+shining= True
+
+def end_handler(signal, frame):
+    global shining
+    print("end of shining")
+    shining = False
+
 def initLEDPin(pin):
     GPIO.setup(pin, GPIO.OUT)
     pwm = GPIO.PWM(pin, 1000)
     pwm.start(0)
     return pwm
-
-def getSignal(pin):
-    start, stop = 0, 0
-    signals = []
-
-    while True:
-        while GPIO.input(pin) == 0:
-            None
-    
-        start = time.time()
-    
-        while GPIO.input(pin) == 1:
-            stop = time.time()
-            duringUp = stop - start
-            if duringUp > 0.1 and len(signals) > 0:
-                return signals[1:]
-    
-        signals.append(duringUp)
-
-def compairSignal(s1, s2, rang):
-    min_len = min(len(s1), len(s2))
-
-    for i in range(min_len):
-        if abs(s1[i] - s2[i]) > rang:
-            return False
-    
-    return True
-
-def decodeSignal(s, signal_map, rang):
-    for name in signal_map.keys():
-        if compairSignal(s, signal_map[name], rang):
-            return name
-
-    return None
 #-----------------------MAIN------------------------#
 def main():
-
-# read key_map
-    src = open("./key_map.json", 'r')
-    signal_map = json.loads(src.read())
-    src.close()
-
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setwarnings(False)
-
-# Set IR_PIN
-    GPIO.setup(37, GPIO.IN)
-
-# init LED_PIN
-    pwmr = initLEDPin(3)
-    pwmg = initLEDPin(5)
-    
-# function introduce
-    print("1    :LED on\n0    :LED off\nPower:poweroff\n\
-+    :Plus Duty\n-    :Minus Duty")
-
-    # start .........
-    while True:  
-        s = getSignal(37)
-        sig=decodeSignal(s,signal_map,0.001)
-        
-        if sig == str('on'):
-            print('----LED on----\n')
-            duties=50
-            pwmr.ChangeDutyCycle(duties)
-            pwmg.ChangeDutyCycle(duties)
-            shining=True
-            while shining:
-                ss = getSignal(37)
-                sigg=decodeSignal(ss,signal_map,0.001)
-                
-                if sigg == str('plus'):
-                    if duties==100:
-                        print('duty limit :100')
-                        continue
-                    duties+=10
-                    print('duty++  now duty:',duties)
-                    pwmr.ChangeDutyCycle(duties)
-                    pwmg.ChangeDutyCycle(duties)
-                
-                elif sigg == str('minus'):
-                    if duties==0:
-                        print('duty limit :0')
-                        continue
-                    duties-=10
-                    print('duty--  now duty:',duties)
-                    pwmr.ChangeDutyCycle(duties)
-                    pwmg.ChangeDutyCycle(duties)
-                
-                elif sigg == str('off'):
-                    print('----LED off----\n')
-                    duties=0
-                    pwmr.ChangeDutyCycle(duties)
-                    pwmg.ChangeDutyCycle(duties)
-                    shining=False
-                elif sigg == str('power'):
-                    print('Turn off the led first')
-
-        elif sig == str('plus'):print('Turn on the led first')
-        elif sig == str('minus'):print('Turn on the led first')
-        elif sig == str('off'):print('Led is off now')
-        elif sig == str('power'):
-            print('----Good Bye----\n')
-            break
-        
-        else:print('not record\n')
-    
+    signal.signal(signal.SIGINT, end_handler)
+    BCM_PIN = 12
+    pwmr=initLEDPin(3)
+    pwmg=initLEDPin(5)
+    pwmb=initLEDPin(7)
+    pwmg.stop()
+    pwmb.stop()
+    while shining:
+        h, t = DHT.read_retry(11, BCM_PIN)
+        if (t<30):
+            duty=1
+        elif (t>=30):
+            duty=100
+        pwmr.ChangeDutyCycle(0)
+        print('now t: ',t,'\nnow duty: ',duty)
+        time.sleep(5)
     # end
     pwmr.stop()
     pwmg.stop()
+    pwmb.stop()
     GPIO.cleanup()
     
 if __name__ == "__main__":
