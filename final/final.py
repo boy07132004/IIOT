3ef423990704b12b10bd97e43a7fcd494fe2c34b
boy07132004 <boy07132004@gmail.com>
2018-06-18 16:53:50 +0800

    add fan

--- final/final.py
+++ final/final.py
@@ -11,35 +11,39 @@ def end_handler(signal, frame):
     print("end of shining")
     shining = False
 
-def initLEDPin(pin):
+def initPin(pin):
     GPIO.setup(pin, GPIO.OUT)
     pwm = GPIO.PWM(pin, 1000)
     pwm.start(0)
     return pwm
 #-----------------------MAIN------------------------#
-def main():
+def mode_1():
     GPIO.setmode(GPIO.BOARD)
     signal.signal(signal.SIGINT, end_handler)
     BCM_PIN = 18
-    pwmr=initLEDPin(3)
-    pwmg=initLEDPin(5)
-    pwmb=initLEDPin(7)
-    pwmg.stop()
-    pwmb.stop()
+    pwmr=initPin(3)
+    pwmg=initPin(5)
+    pwmfan = initPin(40)
+
     while shining:
         h, t = DHT.read_retry(11, BCM_PIN)
         if (t<30):
-            duty=1
+            pwmg.ChangeDutyCycle(70)
+            pwmr.ChangeDutyCycle(0)
+            pwmfan.ChangeDutyCycle(0)
         elif (t>=30):
-            duty=100
-        pwmr.ChangeDutyCycle(duty)
+            duty=5*t-150
+            if duty>100:duty=100
+            pwmg.ChangeDutyCycle(0)
+            pwmr.ChangeDutyCycle(duty)
+            pwmfan.ChangeDutyCycle(duty)
+
         print('now t: ',t,'\nnow duty: ',duty)
         time.sleep(5)
     # end
     pwmr.stop()
     pwmg.stop()
-    pwmb.stop()
     GPIO.cleanup()
     
 if __name__ == "__main__":
-    main()
+    mode_1()
