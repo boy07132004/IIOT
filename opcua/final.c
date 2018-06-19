1db45449b0ec747b81a369c2b95fe2167ab66e5d
boy07132004 <boy07132004@gmail.com>
2018-06-19 14:57:32 +0800

    0619

--- opcua/final.c
+++ opcua/final.c
@@ -1,42 +1,53 @@
 #include </usr/include/python3.5/Python.h>
 #include <signal.h>
 #include "open62541.h"
+#include "unistd.h"
+#include <stdlib.h>
 
 UA_Double tmp=0;
 UA_Double hum=0;
+pid_t child;
 
-//  DHT11  CALLBACK
-    static UA_StatusCode DHTcallback(UA_Server *server,
+//  Mode_1  CALLBACK
+    static UA_StatusCode Mode_1_callback(UA_Server *server,
                          const UA_NodeId *sessionId, void *sessionHandle,
                          const UA_NodeId *methodId, void *methodContext,
                          const UA_NodeId *objectId, void *objectContext,
                          size_t inputSize, const UA_Variant *input,
                          size_t outputSize, UA_Variant *output) {
-        
-        PyObject *pModule = NULL, *pFunc2 = NULL, *pFunc = NULL,*pArg = NULL, *resultH = NULL, *resultM = NULL; 
-    
-        pModule = PyImport_ImportModule ("DHT");
-        pFunc= PyObject_GetAttrString (pModule, "H");
-        resultH = PyEval_CallObject(pFunc, pArg);
-        hum=PyFloat_AsDouble(resultH);
-        pFunc2= PyObject_GetAttrString (pModule, "M");
-        resultM = PyEval_CallObject(pFunc2, pArg);
-        tmp=PyFloat_AsDouble(resultM);
-        printf("Humidity : %f\nTemperature : %f\n",hum,tmp);
-        //  REFRESH Temp
-        UA_NodeId CHANGEId = UA_NODEID_STRING(1, "DHT-Variable");
-        UA_Variant myVarTMP;
-        UA_Variant_init(&myVarTMP);
-        UA_Variant_setScalar(&myVarTMP, &tmp, &UA_TYPES[UA_TYPES_DOUBLE]);
-        UA_Server_writeValue(server, CHANGEId, myVarTMP);
-        //  REFRESH HUM
-        UA_NodeId CHANGEHUMId = UA_NODEID_STRING(0, "DHT-Variable");
-        UA_Variant myVarHUM;
-        UA_Variant_init(&myVarHUM);
-        UA_Variant_setScalar(&myVarHUM, &hum, &UA_TYPES[UA_TYPES_DOUBLE]);
-        UA_Server_writeValue(server, CHANGEHUMId, myVarHUM);
-        //
-        return UA_STATUSCODE_GOOD;}
+        if(child == 0) {
+            PyRun_SimpleString("GPIO.cleanup()");
+            exit(0);
+        }
+        child = fork();
+        if (child == 0){
+            printf("Start Mode_1");
+            system('python3 ./mode_1.py');
+        }
+        else if (child >0)return UA_STATUSCODE_GOOD;
+        else printf("error!!\n");
+    }
+
+//  Mode_2  CALLBACK
+    static UA_StatusCode Mode_2_callback(UA_Server *server,
+                         const UA_NodeId *sessionId, void *sessionHandle,
+                         const UA_NodeId *methodId, void *methodContext,
+                         const UA_NodeId *objectId, void *objectContext,
+                         size_t inputSize, const UA_Variant *input,
+                         size_t outputSize, UA_Variant *output) {
+        if(child == 0) {
+            PyRun_SimpleString("GPIO.cleanup()");
+            exit(0);
+        }
+        child = fork();
+        if (child == 0){
+            printf("Start Mode_2");
+            system('python3 ./mode_2.py');
+        }
+        else if (child >0)return UA_STATUSCODE_GOOD;
+        else printf("error!!\n");
+    }
+
 //  DHT11  Add OBJECT
     static void addObjectDHT(UA_Server *server) {
     UA_ObjectAttributes HAttr = UA_ObjectAttributes_default;
@@ -48,22 +59,8 @@ UA_Double hum=0;
                             UA_QUALIFIEDNAME(1, "DHT"),
                             UA_NODEID_NULL,HAttr, NULL, NULL);
     
-    //==VARIABLE==//
-        UA_VariableAttributes HUMattr = UA_VariableAttributes_default;
-        UA_Double HUM = hum;
-        UA_Variant_setScalar(&HUMattr.value, &HUM, &UA_TYPES[UA_TYPES_DOUBLE]);
-        HUMattr.description = UA_LOCALIZEDTEXT("en-US","the answer");
-        HUMattr.displayName = UA_LOCALIZEDTEXT("en-US","Hum");
-        HUMattr.dataType = UA_TYPES[UA_TYPES_DOUBLE].typeId;
-        HUMattr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
-        UA_QualifiedName HUMName = UA_QUALIFIEDNAME(1, "the answer");
-        UA_NodeId humnodeid =UA_NODEID_STRING(0, "DHT-Variable");
-        UA_NodeId parentReferenceNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES);
-        UA_Server_addVariableNode(server, humnodeid, DOBJNodeId,
-                                parentReferenceNodeId, HUMName,
-                                UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), HUMattr, NULL, NULL);
     
-    //==VARIABLE2==//
+    //==VARIABLE==//
         UA_VariableAttributes TEMPattr = UA_VariableAttributes_default;
         UA_Double TEMP = tmp;
         UA_Variant_setScalar(&TEMPattr.value, &TEMP, &UA_TYPES[UA_TYPES_DOUBLE]);
@@ -72,21 +69,33 @@ UA_Double hum=0;
         TEMPattr.dataType = UA_TYPES[UA_TYPES_DOUBLE].typeId;
         TEMPattr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
         UA_QualifiedName TEMPName = UA_QUALIFIEDNAME(1, "the answer");
-        UA_NodeId tmpnodeid =UA_NODEID_STRING(1, "DHT-Variable");
+        UA_NodeId tmpnodeid =UA_NODEID_STRING(0, "DHT-Variable");
         UA_Server_addVariableNode(server, tmpnodeid, DOBJNodeId,
                                 UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES), TEMPName,
                                 UA_NODEID_NUMERIC(0, UA_NS0ID_BASEDATAVARIABLETYPE), TEMPattr, NULL, NULL);
-    //==METHOD==//
-        UA_MethodAttributes Hattr = UA_MethodAttributes_default;
-        Hattr.description = UA_LOCALIZEDTEXT("en-US","DHTdata");
-        Hattr.displayName = UA_LOCALIZEDTEXT("en-US","Get Data");
-        Hattr.executable = true;
-        Hattr.userExecutable = true;
+    //==MODE_1==//
+        UA_MethodAttributes MODE1_ATTR = UA_MethodAttributes_default;
+        MODE1_ATTR.description = UA_LOCALIZEDTEXT("en-US","Auto");
+        MODE1_ATTR.displayName = UA_LOCALIZEDTEXT("en-US","Mode_1");
+        MODE1_ATTR.executable = true;
+        MODE1_ATTR.userExecutable = true;
+        UA_Server_addMethodNode(server, UA_NODEID_NUMERIC(0,62542),
+                                DOBJNodeId,
+                                UA_NODEID_NUMERIC(0, UA_NS0ID_HASORDEREDCOMPONENT),
+                                UA_QUALIFIEDNAME(1, "DHT-QualName"),
+                                MODE1_ATTR, &Mode_1_callback,
+                                0, NULL, 0, NULL, NULL, NULL);
+
+        UA_MethodAttributes MODE2_ATTR = UA_MethodAttributes_default;
+        MODE2_ATTR.description = UA_LOCALIZEDTEXT("en-US","Auto");
+        MODE2_ATTR.displayName = UA_LOCALIZEDTEXT("en-US","Mode_2");
+        MODE2_ATTR.executable = true;
+        MODE2_ATTR.userExecutable = true;
         UA_Server_addMethodNode(server, UA_NODEID_NUMERIC(1,62542),
                                 DOBJNodeId,
                                 UA_NODEID_NUMERIC(0, UA_NS0ID_HASORDEREDCOMPONENT),
                                 UA_QUALIFIEDNAME(1, "DHT-QualName"),
-                                Hattr, &DHTcallback,
+                                MODE2_ATTR, &Mode_2_callback,
                                 0, NULL, 0, NULL, NULL, NULL);
     }
 
