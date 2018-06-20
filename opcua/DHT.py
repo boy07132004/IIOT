08781baa3b11618d76647187e6a2bc79dc45bb57
boy07132004 <boy07132004@gmail.com>
2018-06-20 18:06:50 +0800

    dht.py

--- opcua/DHT.py
+++ opcua/DHT.py
@@ -4,10 +4,10 @@ import Adafruit_DHT as DHT
 def H():
     h=0
     t=0
-    h, t = DHT.read_retry(11, 17)
+    h, t = DHT.read_retry(11, 18)
     return float(h)
 def M():
     h=0
     t=0
-    h, t = DHT.read_retry(11, 17)
+    h, t = DHT.read_retry(11, 18)
     return float(t)
