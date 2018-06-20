61a6eac341a31e819ea6d5a0c26a01ca5fdc3fff
boy07132004 <boy07132004@gmail.com>
2018-06-20 17:37:20 +0800

    TERM

--- opcua/final.c
+++ opcua/final.c
@@ -18,7 +18,7 @@ int execres = 100;
                          const UA_NodeId *objectId, void *objectContext,
                          size_t inputSize, const UA_Variant *input,
                          size_t outputSize, UA_Variant *output) {
-            if (!died) kill(child,SIG_IGN);
+            if (!died) kill(child,SIGTERM);
             child = fork();
             died =false;
             if (child ==0){
